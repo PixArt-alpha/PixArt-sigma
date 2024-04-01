3ff9674ac7f65d495722042325995b7456f2e0e5
chenjunsong <cjs1020440147@icloud.com>
2024-04-01 13:28:37 +0800

    CAME & SA bug fixed;

--- diffusion/data/datasets/__init__.py
+++ diffusion/data/datasets/__init__.py
@@ -1,5 +1,3 @@
-from .SA import SAM
 from .InternalData import InternalData, InternalDataSigma
 from .InternalData_ms import InternalDataMS, InternalDataSigma
-from .Dreambooth import DreamBooth
 from .utils import *
