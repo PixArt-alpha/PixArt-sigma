afa1b5c4d32814b97f5eee102dd34e63923a7cad
frutiemax92 <142428698+frutiemax92@users.noreply.github.com>
2024-04-18 06:59:31 -0400

    Create tmp folder in the output folder to avoid errors (#41)
    * Create tmp folder in the output folder to avoid errors
    
    * reformat
    
    ---------
    
    Co-authored-by: chenjunsong <cjs1020440147@icloud.com>

--- train_scripts/train.py
+++ train_scripts/train.py
@@ -363,6 +363,7 @@ if __name__ == '__main__':
             "A photo of beautiful mountain with realistic sunset and blue lake, highly detailed, masterpiece",
         ]
         skip = True
+        Path('output/tmp').mkdir(parents=True, exist_ok=True)
         for prompt in validation_prompts:
             if not (os.path.exists(f'output/tmp/{prompt}_{max_length}token.pth')
                     and os.path.exists(f'output/pretrained_models/null_embed_diffusers_{max_length}token.pth')):
