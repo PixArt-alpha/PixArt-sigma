3276026bf65e806aadc5c35ab21ab5c9b53ae546
chenjunsong <cjs1020440147@icloud.com>
2024-04-23 11:48:36 +0800

    reformat

--- train_scripts/train.py
+++ train_scripts/train.py
@@ -195,8 +195,7 @@ def train():
 
                 info = f"Step/Epoch [{global_step}/{epoch}][{step + 1}/{len(train_dataloader)}]:total_eta: {eta}, " \
                     f"epoch_eta:{eta_epoch}, time_all:{t:.3f}, time_data:{t_d:.3f}, lr:{lr:.3e}, "
-                if hasattr(model, 'module'):
-                    info += f's:({model.module.h}, {model.module.w}), '
+                info += f's:({model.module.h}, {model.module.w}), ' if hasattr(model, 'module') else f's:({model.h}, {model.w}), '
 
                 info += ', '.join([f"{k}:{v:.4f}" for k, v in log_buffer.output.items()])
                 logger.info(info)
