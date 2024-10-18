d1d9b856e00b499a12a47c0367f72d5d5961af51
TheNetherWatcher <117659673+TheNetherWatcher@users.noreply.github.com>
2024-10-19 00:27:46 +0530

    Fixed error when calculating latents using VAE
    VAE is expecting 4 values, i.e., (batch_size, channels, height, width), but we are only passing (channels, height, width), hence adding an extra dimension in the training vectors before generating latent vectors from VAE

--- train_scripts/train_pixart_lora_hf.py
+++ train_scripts/train_pixart_lora_hf.py
@@ -810,7 +810,7 @@ def main():
         for step, batch in enumerate(train_dataloader):
             with accelerator.accumulate(transformer):
                 # Convert images to latent space
-                latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample()
+                latents = vae.encode(batch["pixel_values"].unsqueeze(0).to(dtype=weight_dtype)).latent_dist.sample()
                 latents = latents * vae.config.scaling_factor
 
                 # Sample noise that we'll add to the latents
